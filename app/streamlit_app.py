da9e6840601e6297a1a35e87c83810c2fc8343f2
Kepler Warrington-Arroyo <kepler.warrington-arroyo@epfl.ch>
2023-03-25 08:11:40 +0100

    Added logging redirects

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from utils.setup import *
+from utils.console import *
 
 st.title("Hello World!")
 
-fetch_LELSD_requirements()
+with st_stdout("info"):
+    fetch_LELSD_requirements()
